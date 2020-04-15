d40f9ec0171b0ab16705494f2fa01b1833121439
sdesousa <sylvaindesousa@free.fr>
2020-04-15 10:45:53 +0200

    creation manga entity

---
+++ src/Entity/Manga.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MangaRepository")
+ */
+class Manga
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Titre obligatoire")
+     * @Assert\Length(
+     *      max = 255,
+     *      maxMessage = "Titre trop long, il doit être au plus {{ limit }} caractères"
+     * )
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Positive(message="Dois être positif")
+     */
+    private $totalVolume;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\PositiveOrZero(message="Dois être positif")
+     */
+    private $availableVolume;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\GreaterThanOrEqual(
+     *     value="1900",
+     *     message="Année invalide"
+     * )
+     */
+    private $year;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTotalVolume(): ?int
+    {
+        return $this->totalVolume;
+    }
+
+    public function setTotalVolume(?int $totalVolume): self
+    {
+        $this->totalVolume = $totalVolume;
+
+        return $this;
+    }
+
+    public function getAvailableVolume(): ?int
+    {
+        return $this->availableVolume;
+    }
+
+    public function setAvailableVolume(?int $availableVolume): self
+    {
+        $this->availableVolume = $availableVolume;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(?int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+}
