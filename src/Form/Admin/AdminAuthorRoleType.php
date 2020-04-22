0586d75e638deeb017dd4a7beef86b79dc922e15
sdesousa <sylvaindesousa@free.fr>
2020-04-22 15:32:51 +0200

    admin authorRole

---
+++ src/Form/Admin/AdminAuthorRoleType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\AuthorRole;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminAuthorRoleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('role', TextType::class, [
+                'label' => 'Rôle',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => AuthorRole::class,
+        ]);
+    }
+}
